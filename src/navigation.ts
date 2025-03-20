5b3432339bfc750f46a8c22e6432d984fbdf6367
kokitree <hello@kokitree.com>
2025-03-20 18:19:21 +0700

    footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -123,7 +123,6 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+ · All rights reserved.
   `,
 };
